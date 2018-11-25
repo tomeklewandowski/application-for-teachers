c1c320246c0641bcd9b20447df1bb81ee5384cd1
tomeklewandowski <lewytomek@gmail.com>
2018-11-25 12:05:25 +0100

    Add admin

--- classes/exams/models.py
+++ classes/exams/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from teams.models import CourseType
 
-# Create your models here.
+
+class Exam(models.Model):
+    course_type = models.ForeignKey(
+        CourseType,
+        verbose_name="Typ kursu",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    name = models.CharField(
+        verbose_name="Nazwa",
+        max_length=100
+    )
+    module_num = models.PositiveIntegerField(
+        verbose_name="Nr modułu"
+    )
--- classes/teams/admin.py
+++ classes/teams/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CourseType, Group, Student
 
-# Register your models here.
+
+@admin.register(CourseType)
+class CourseTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
